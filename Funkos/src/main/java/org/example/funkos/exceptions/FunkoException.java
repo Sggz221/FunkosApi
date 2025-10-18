9b8cff628ba4d7b873d9da8cbffe76dc2c6cb207
Sggz221 <sggz221@gmail.com>
2025-10-18 13:29:25 +0200

    Soporte para logs implementado

--- Funkos/src/main/java/org/example/funkos/exceptions/FunkoException.java
+++ Funkos/src/main/java/org/example/funkos/exceptions/FunkoException.java
@@ -7,16 +7,4 @@ public sealed class FunkoException extends RuntimeException {
     public static final class NotFoundException extends FunkoException {
         public NotFoundException(String message) { super(message); }
     }
-
-    public static final class InvalidException extends FunkoException {
-        public InvalidException(String message) { super(message); }
-    }
-
-    public static final class DatabaseException extends FunkoException {
-        public DatabaseException(String message) { super(message); }
-    }
-
-    public static final class ApiException extends FunkoException {
-        public ApiException(String message) { super(message); }
-    }
 }
